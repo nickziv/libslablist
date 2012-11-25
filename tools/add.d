d1ec3dc7c57d8a2ad223f025c4bd9ed687c174df
Nick Zivkovic <zivkovic.nick@gmail.com>
2012-11-25 01:09:25 -0600

    add.d can be cleaner, and we should use vtimestamp

--- tools/add.d
+++ tools/add.d
@@ -1,40 +1,14 @@
-/*
-slablist$target:::add_begin,
-slablist$target:::rem_begin
-{
-	self->name = (args[0]->sli_name);
-	self->sml = (args[0]->sli_is_small_list);
-	self->pn = probename;
-}
-*/
-
-/*
-slablist$target::small_list_add:got_here
-/self->name == "intlistsrt"/
-{
-	trace(arg0);
-}
-*/
-
-/*
-pid$target::cmpfun:entry,
-pid$target::cmpfun_str:entry
-{
-	@[self->name, self->sml, self->pn, probefunc] = count();
-}
-*/
-
 slablist$target:::add_begin
 {
-	self->ts = timestamp;
-	self->elems = args[0]->sli_slabs;
+	self->ts = vtimestamp;
+	self->elems = args[0]->sli_elems;
 	self->name = args[0]->sli_name;
 	self->type = args[0]->sli_is_small_list;
 }
 
 slablist$target:::add_end
 {
-	self->ets = timestamp;
+	self->ets = vtimestamp;
 	@avgt["avg", self->name, self->type] = avg(self->ets - self->ts);
 	@qtzt["qtz", self->name, self->type] = quantize(self->ets - self->ts);
 }
